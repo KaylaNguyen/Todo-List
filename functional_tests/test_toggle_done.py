12a9355a52fc3ba1a8e12681eba6de6dde8f9dea
Kayla Nguyen <nguye27k@mtholyoke.edu>
2016-01-13 14:41:22 -0500

    Add toggle item when done.

--- functional_tests/test_toggle_done.py
+++ functional_tests/test_toggle_done.py
@@ -1,5 +1,6 @@
 from .base import TodoFunctionalTest
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 class ToggleDoneTest(TodoFunctionalTest):
     def test_can_toggle_finished_items(self):
@@ -37,7 +38,11 @@ class ToggleDoneTest(TodoFunctionalTest):
     def toggle_todo_done(self, todo_text):
         row = self.find_table_row(todo_text)
         row.find_element_by_tag_name('input').click()
-        self.browser.find_element_by_id('toggle_done')
+        self.browser.find_element_by_id('toggle_done').click()
 
     def check_marked_off(self, todo_text):
-        pass
+        row = self.find_table_row(todo_text)
+        try:
+            row.find_element_by_css_selector('.todo-done')
+        except NoSuchElementException:
+            self.fail("%s not marked done!" % (todo_text))
