a1f8f80d843045df91ff11ed02940aa375729873
Kayla Nguyen <nguye27k@mtholyoke.edu>
2016-01-14 12:08:02 -0500

    Add toggle and delete item functions.

--- functional_tests/test_toggle_done.py
+++ functional_tests/test_toggle_done.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
 class ToggleDoneTest(TodoFunctionalTest):
-    def test_can_toggle_finished_items(self):
+    def test_can_mark_finished_items(self):
         # Edith makes a quick shopping list.
         # noticing a checkbox to toggle done items
         self.browser.get(self.live_server_url)
@@ -34,6 +34,39 @@ class ToggleDoneTest(TodoFunctionalTest):
         self.toggle_todo_done(['Tie some flys'])
         self.check_marked_off('Tie some flys')
 
+    def test_can_toggle_finished_items(self):
+        # Edith's ties flying hobby is booming, and she wants
+        # a list that she can use over and over
+        self.browser.get(self.live_server_url)
+        self.enter_a_new_item('Buy feathers')
+        self.enter_a_new_item('Buy fishing line')
+        self.enter_a_new_item('Buy sparkles')
+
+        # She looks in her closet, and already has fishing line
+        self.toggle_todo_done(['Buy fishing line'])
+        self.check_marked_off('Buy fishing line')
+
+        # She goes to the store, and finishes shopping
+        self.toggle_todo_done([
+            'Buy feathers',
+            'Buy sparkles',
+        ])
+
+        self.check_marked_off('Buy feathers')
+        self.check_marked_off('Buy fishing line')
+        self.check_marked_off('Buy sparkles')
+
+        # She makes some flys, and her closet is empty
+        self.toggle_todo_done([
+            'Buy feathers',
+            'Buy fishing line',
+            'Buy sparkles',
+        ])
+
+        self.check_not_marked_off('Buy feathers')
+        self.check_not_marked_off('Buy fishing line')
+        self.check_not_marked_off('Buy sparkles')
+
     def toggle_todo_done(self, todo_text_list):
         for todo_text in todo_text_list:
             row = self.find_table_row(todo_text)
@@ -46,3 +79,10 @@ class ToggleDoneTest(TodoFunctionalTest):
             row.find_element_by_css_selector('.todo-done')
         except NoSuchElementException:
             self.fail("%s not marked done!" % (todo_text))
+
+    def check_not_marked_off(self, todo_text):
+        try:
+            self.check_marked_off(todo_text)
+        except:
+            return
+        self.fail("'%s' is marked done!" % (todo_text))
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -156,7 +156,7 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'input type="checkbox"')
 
-    def test_POST_items_toggles_done(self):
+    def test_POST_items_marked_done(self):
         # create list and items
         current_list = List.objects.create()
         item1 = Item.objects.create(text="Item 1", list=current_list)
@@ -205,3 +205,32 @@ class ListViewTest(TestCase):
         item2 = Item.objects.get(id=item2.id)
         self.assertFalse(item1.is_done)
         self.assertFalse(item2.is_done)
+
+    def test_POST_items_toggles_done(self):
+        # create list and items
+        current_list = List.objects.create()
+        item1 = Item.objects.create(
+            text="Item 1",
+            list=current_list,
+            is_done=True
+        )
+
+        item2 = Item.objects.create(
+            text="Item 2",
+            list=current_list,
+            is_done=False
+        )
+
+        # POST data
+        response = self.client.post(
+            '/lists/%d/items/' % (current_list.id),
+            data={'mark_item_done': item2.id}
+        )
+        # including toggle item
+        self.assertRedirects(response, '/lists/%d/' % (current_list.id))
+
+        # check the item is updated
+        item1 = Item.objects.get(id=item1.id)
+        item2 = Item.objects.get(id=item2.id)
+        self.assertFalse(item1.is_done)
+        self.assertTrue(item2.is_done)
